c737755d9e26d764ff552a844fbb00fc83e67a64
tquinonero <tquinonero.web@gmail.com>
2026-02-22 01:18:28 +0100

    Added color palette generator tool

--- src/utils/getPageName.ts
+++ src/utils/getPageName.ts
@@ -6,6 +6,7 @@ export function getPageName(pathname: string): string {
   if (pathname.includes('/frontmatter-generator')) return 'frontmatter-generator';
   if (pathname.includes('/wordpress-quiz')) return 'wordpress-quiz';
   if (pathname.includes('/regex-tester')) return 'regex-tester';
+  if (pathname.includes('/color-palette-generator')) return 'color-palette-generator';
   if (pathname.includes('/blog')) return 'blog';
   if (pathname.includes('/about')) return 'about';
   if (pathname.includes('/contact')) return 'contact';
