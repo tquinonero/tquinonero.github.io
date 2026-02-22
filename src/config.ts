b552d0aa027225d505c3219b331d23ea0dabd5ae
tquinonero <tquinonero.web@gmail.com>
2026-02-22 02:38:17 +0100

    Added new tools to tool city

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
  */
 
 export const SITE_CONFIG = {
-  title: 'Astro Website',
+  title: 'Tquinonero · WordPress & Astro Web Developer',
   email: 'tquinonero.web@gmail.com',
   github: 'https://github.com/tquinonero?tab=repositories',
   description: 'Portfolio and web development projects',
